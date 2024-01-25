f9af57bdc2bbcd5f7ee75dc42496a668b39b8d29
AdmiralKeso <128307688+AdmiralKeso@users.noreply.github.com>
2024-01-25 14:30:12 +0100

    Add player move backgrounds depending on choice

--- assets/script/script.js
+++ assets/script/script.js
@@ -30,18 +30,23 @@ function start(playerMove){
     else{
         switch(playerMove){ //Player move compared to computer move.
             case "rock":
+                document.getElementById("playmove").style.backgroundImage = "url(/assets/images/rock.jpg)"
                 result = (computerMove === "scissors","cyclops") ? 'YOU WIN!' : 'YOU LOSE!'; //Player picks cyclops. Computer picks scissors, cycplops = win. If not = false (You lose)
 
             case "paper":
+                document.getElementById("playmove").style.backgroundImage = "url(/assets/images/paper.jpg)"
                 result = (computerMove === "rock","spock") ? 'YOU WIN!' : 'YOU LOSE!'; //Player picks cyclops. Computer picks rock, spock = win. If not = false (You lose)
 
             case "scissors":
+                document.getElementById("playmove").style.backgroundImage = "url(/assets/images/scissors.png)"
                 result = (computerMove === "paper","cyclops") ? 'YOU WIN!' : 'YOU LOSE!'; //Player picks cyclops. Computer picks cyclops, paper = win. If not = false (You lose)
 
             case "cyclops":
+                document.getElementById("playmove").style.backgroundImage = "url(/assets/images/cyclops.png)"
                 result = (computerMove === "spock","paper") ? 'YOU WIN!' : 'YOU LOSE!'; //Player picks cyclops. Computer picks spock, paper = win. If not = false (You lose)
 
             case "spock":
+                document.getElementById("playmove").style.backgroundImage = "url(/assets/images/spock.png)"
                 result = (computerMove === "scissors","rock") ? 'YOU WIN!' : 'YOU LOSE!'; //Player picks cyclops. Computer picks scissors, rock = win. If not = false (You lose)
     }
 }
